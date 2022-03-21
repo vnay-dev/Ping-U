5397992fe09639a7bdb2119bd7104179745a6287
vnay-dev <cleancode4dev@gmail.com>
2022-03-21 14:15:47 +0530

    fetch messages

---
+++ backend/controllers/messages.js
@@ -0,0 +1,50 @@
+const asyncHandler = require("express-async-handler");
+const Chats = require("../models/chats");
+const Message = require("../models/message");
+const Users = require("../models/user");
+
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid request data");
+    return res.sendStatus(400);
+  }
+
+  const newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+    message = await message.populate("sender", "name picture");
+    message = await message.populate("chats");
+    message = await Users.populate(message, {
+      path: "chat.users",
+      select: "name picture email",
+    });
+    await Chats.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    });
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const fetchMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name picture email")
+      .populate("chats");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { sendMessage, fetchMessages };
--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { sendMessage, fetchMessages } = require("../controllers/messages");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
 
-//router.post("/", protect, sendMessage);
-//router.get("/:chatId", protect, fetchMessages); // fetch all messages w.r.t chatId
+router.post("/", protect, sendMessage);
+router.get("/:chatId", protect, fetchMessages); // fetch all messages w.r.t chatId
 
 module.exports = router;
